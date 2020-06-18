d435a2136d9ea320175b104bb4d3bdd62d27fa78
Reza Fatahi <aug2uag@gmail.com>
2020-06-17 23:57:41 -0700

    Scotty inc endpoint

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# HaskellGHC
+# Object Security Interview
+
+Author: Reza Fatahi<br>
+Start date: June 17, 2020 approximately 5:00PM PST<br>
+End date: June 16, 2020 11:55PM PST<br>
+Total time spent: approximately 4.0 hours (non-continuous)
+
+
+### About
+
+This project is made with Haskell stack and Scotty web API.
+
+
+### Running App
+
+This app runs on port 3000.
+
+In this directory:
+
+1) `$ stack build`
+2) `$ stack ghci `
+3) `*Main> main `
--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Web.Scotty
 import System.IO (readFile, writeFile)
+
+main = do
+    putStrLn "Starting Server..."
+    scotty 3000 $ do
+        get "/value" $ do
+            let i = incrementFile
+            text "Ok"
+
+
 
 numFromFile :: IO String
 numFromFile = do
     content <- readFile "num"
     return (content)
 
-main = do
-    putStrLn "hello"
+incrementFile = do
     val <- numFromFile
     let num = succ $ read val :: Integer
     let str = show $ num :: String
